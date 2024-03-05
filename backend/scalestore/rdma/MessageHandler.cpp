45f74c50a8c1feb67ff93777bae180f5bba57014
“Yuval-F” <yuval@frnzx.io>
2024-03-05 16:16:22 +0100

    adding ensure to see

--- backend/scalestore/rdma/MessageHandler.cpp
+++ backend/scalestore/rdma/MessageHandler.cpp
@@ -403,15 +403,14 @@ void MessageHandler::startThread() {
                   case MESSAGE_TYPE::CUSFR: {
                       auto& request = *reinterpret_cast<CreateOrUpdateShuffledFrameRequest*>(ctx.request);
                       PID shuffledPid = PID(request.shuffledPid);
-                      pageIdManager.addPageWithExistingPageId(request.shuffledPid, request.pageEvictedAtOldNode);
                       auto guard = bm.findFrameOrInsert<CONTENTION_METHOD::NON_BLOCKING>(shuffledPid, Exclusive(), ctx.bmId,true);
                       if(guard.state == STATE::RETRY){ // this it to deal with a case of the distrubted deadlock
-                          pageIdManager.removePage(request.shuffledPid);
                           auto& response = *MessageFabric::createMessage<rdma::CreateOrUpdateShuffledFrameResponse>(ctx.response);
                           response.accepted = false;
                           writeMsg(clientId, response, threads::ThreadContext::my().page_handle);
                           break;
                       }
+                      pageIdManager.addPageWithExistingPageId(request.shuffledPid, request.pageEvictedAtOldNode);
                       guard.frame->possession = request.possession;
                       if(request.possession == POSSESSION::SHARED){
                           guard.frame->possessors.shared.bitmap = request.possessors;
