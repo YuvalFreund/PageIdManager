984f9672f9001ec1f94500adfeb786504b3d6e3c
“Yuval-F” <yuval@frnzx.io>
2024-03-14 14:20:02 +0100

    remove shuffling to check if ssd is issue

--- frontend/ycsb/ycsb.cpp
+++ frontend/ycsb/ycsb.cpp
@@ -21,7 +21,7 @@ DEFINE_bool(YCSB_record_latency, false, "");
 DEFINE_bool(YCSB_all_zipf, false, "");
 DEFINE_bool(YCSB_local_zipf, false, "");
 DEFINE_bool(YCSB_flush_pages, false, "");
-DEFINE_uint32(YCSB_shuffle_ratio, 10, "");
+DEFINE_uint32(YCSB_shuffle_ratio, 20, "");
 // -------------------------------------------------------------------------------------
 using u64 = uint64_t;
 using u8 = uint8_t;
