55fd0b1446a42e1318a701629d66fefd98c8aaca
“Yuval-F” <yuval@frnzx.io>
2024-08-22 09:53:47 +0200

    change defs to fit nodes ip

--- frontend/ycsb/ycsb.cpp
+++ frontend/ycsb/ycsb.cpp
@@ -280,7 +280,7 @@ int main(int argc, char* argv[])
                            // todo yuval - stops processing any transactions
                        }
                        */
-                       if(false){//scalestore.getNodeID() == leavingNodeId && pageIdManager.isBeforeShuffle == false && utils::RandomGenerator::getRandU64(0, 100) < shuffleRatio ){//&& (t_i == 0 ||t_i==1 ) ) { // worker will go and shuffle
+                       if(false ){//scalestore.getNodeID() == leavingNodeId && pageIdManager.isBeforeShuffle == false && utils::RandomGenerator::getRandU64(0, 100) < shuffleRatio ){//&& (t_i == 0 ||t_i==1 ) ) { // worker will go and shuffle
                            finishedShuffling = mh.shuffleFrameAndIsLastShuffle(workerPtr);
                        } else {
                            K key = zipf_random->rand(zipf_offset);
