3186e5839365421e4bc2f3a5d2cfcf42f406a2b9
“Yuval-F” <yuval@frnzx.io>
2024-10-04 16:02:36 +0200

    more fixing to guard - trying at map

--- backend/scalestore/rdma/MessageHandler.cpp
+++ backend/scalestore/rdma/MessageHandler.cpp
@@ -584,7 +584,7 @@ bool MessageHandler::shuffleFrameAndIsLastShuffle(scalestore::threads::Worker* w
     for(int i = 0; i < createdFramesResponse.successfulAmount; i++){
         uint64_t successfulPID = createdFramesResponse.successfulShuffledPid[i];
         pageIdManager.setDirectoryOfPage(successfulPID, newNodeId);
-        auto& successfulGuard = pidToGuardMap[successfulPID];
+        auto& successfulGuard = pidToGuardMap.at(successfulPID);
         if(successfulGuard.frame->isPossessor(pageIdManager.nodeId) == false){
             bm.removeFrame(successfulGuard.frame, [](BufferFrame& /*frame*/) {});
             // guard is unlatched here^^^^^
