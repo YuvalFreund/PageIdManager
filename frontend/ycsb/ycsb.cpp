2ddd5013ea024c1cd9da29db6381955cff7c04f9
“Yuval-F” <yuval@frnzx.io>
2024-10-13 10:59:25 +0200

    removing the page provider function to check it later

--- frontend/ycsb/ycsb.cpp
+++ frontend/ycsb/ycsb.cpp
@@ -286,7 +286,7 @@ int main(int argc, char* argv[])
                                std::chrono::steady_clock::time_point finishShuffling = std::chrono::steady_clock::now();
                                std::cout<<"Done shuffling! shuffle percentage :" << shuffleRatio<< " shuffle time: "<< std::chrono::duration_cast<std::chrono::microseconds>(finishShuffling - beginOfShuffling).count()  <<std::endl;
                                pageIdManager.gossipNodeFinishedShuffling(workerPtr);
-                               pageProvider.changeEvictionRate(100);
+                               //todo - check later pageProvider.changeEvictionRate(100);
                                break;
                            }
                        } else {
